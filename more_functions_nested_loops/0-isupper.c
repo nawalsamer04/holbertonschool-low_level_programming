261b802cf1b1b3936ec57b507082e5fdca45cfd2
root <root@ip-10-42-67-16.ec2.internal>
2025-09-04 20:43:18 +0000

    Task 0 : implement _isupper (betty clean)

--- more_functions_nested_loops/0-isupper.c
+++ more_functions_nested_loops/0-isupper.c
@@ -5,11 +5,11 @@
  * @c: character code to check
  * Return: 1 if c is uppercase, 0 otherwise
  */
- int _isupper(int c)
+int _isupper(int c)
 {
 	if (c >= 'A' && c <= 'Z')
-	{
 		return (1);
-	}
-	return(0);
+	else
+		return (0);
 }
+
--- more_functions_nested_loops/README.md
+++ more_functions_nested_loops/README.md
@@ -1,1 +1,6 @@
-More functions & nested loops
+# More Functions, More Nested Loops
+
+This project includes C functions for:
+- Checking uppercase characters (`_isupper`)
+- More function practice with nested loops and conditions
+
