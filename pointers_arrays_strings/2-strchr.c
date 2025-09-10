1fb98bcbb93a71e99c576b1bbe4c98a63851d9d3
root <root@ip-10-42-255-14.ec2.internal>
2025-09-10 12:35:59 +0000

    Task 2: implement _strchr (Betty-compliant)

---
+++ pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * _strchr - locates a character in a string
+ * @s: pointer to the string
+ * @c: character to find
+ *
+ * Return: pointer to the first occurrence of c in s,
+ *         or NULL if not found
+ */
+char *_strchr(char *s, char c)
+{
+	while (*s != '\0')
+	{
+		if (*s == c)
+			return (s);
+		s++;
+	}
+
+	/* also allow matching the terminating '\0' */
+	if (*s == c)
+		return (s);
+
+	return (NULL);
+}
+
