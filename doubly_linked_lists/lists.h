43d4ab9563805265934b1b270f017b4b7acbc1b4
root <root@ip-10-42-189-56.ec2.internal>
2025-10-16 23:04:15 +0000

    Add prototype and fix compile for get_dnodeint_at_index

--- doubly_linked_lists/lists.h
+++ doubly_linked_lists/lists.h
@@ -4,14 +4,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-/**
- * struct dlistint_s - doubly linked list
- * @n: integer
- * @prev: points to the previous node
- * @next: points to the next node
- *
- * Description: doubly linked list node structure
- */
+/* struct */
 typedef struct dlistint_s
 {
 	int n;
@@ -19,11 +12,15 @@ typedef struct dlistint_s
 	struct dlistint_s *next;
 } dlistint_t;
 
+/* prototypes you should have so far */
 size_t print_dlistint(const dlistint_t *h);
 size_t dlistint_len(const dlistint_t *h);
 dlistint_t *add_dnodeint(dlistint_t **head, const int n);
 dlistint_t *add_dnodeint_end(dlistint_t **head, const int n);
 void free_dlistint(dlistint_t *head);
+
+/* ➜ ADD THIS LINE (exactly) */
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index);
 
 #endif /* LISTS_H */
 
