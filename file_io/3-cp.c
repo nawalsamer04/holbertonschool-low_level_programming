a0c0268ac94ea25497c32469f882ca7fb3b14312
root <root@ip-10-42-103-112.ec2.internal>
2025-10-17 01:24:11 +0000

    Fix read(), Betty; proper exits 97/98/99/100 for 3-cp

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -1,17 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
 #include <fcntl.h>
-#include <sys/stat.h>
+#include <unistd.h>
 
 #define BUF_SIZE 1024
-#define PERMS 0664
 
 /**
  * print_read_error - print read error and exit 98
  * @file: file name
  */
-static void print_read_error(const char *file)
+void print_read_error(const char *file)
 {
 	dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file);
 	exit(98);
@@ -21,17 +19,17 @@ static void print_read_error(const char *file)
  * print_write_error - print write error and exit 99
  * @file: file name
  */
-static void print_write_error(const char *file)
+void print_write_error(const char *file)
 {
 	dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file);
 	exit(99);
 }
 
 /**
- * close_fd - close fd and handle error
- * @fd: file descriptor to close
+ * close_fd - close fd or exit 100 on failure
+ * @fd: file descriptor
  */
-static void close_fd(int fd)
+void close_fd(int fd)
 {
 	if (close(fd) == -1)
 	{
@@ -41,60 +39,46 @@ static void close_fd(int fd)
 }
 
 /**
- * main - copy the content of a file to another file
- * @argc: argument count
- * @argv: argument vector
- *
- * Return: 0 on success, exits with:
- * 97 for wrong usage, 98 for read error, 99 for write error, 100 for close error.
+ * main - copy the content of a file to another
+ * @ac: arg count
+ * @av: arg vector
+ * Return: 0 on success
  */
-int main(int argc, char *argv[])
+int main(int ac, char **av)
 {
 	int fd_from, fd_to;
-	ssize_t rbytes, wbytes;
+	ssize_t r, w;
 	char buf[BUF_SIZE];
 
-	if (argc != 3)
+	if (ac != 3)
 	{
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
 
-	fd_from = open(argv[1], O_RDONLY);
+	fd_from = open(av[1], O_RDONLY);
 	if (fd_from == -1)
-		print_read_error(argv[1]);
+		print_read_error(av[1]);
 
-	fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, PERMS);
+	fd_to = open(av[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 	if (fd_to == -1)
-	{
-		close_fd(fd_from);
-		print_write_error(argv[2]);
-	}
+		print_write_error(av[2]);
 
-	while ((rbytes = read(fd_from, buf, BUF_SIZE)) > 0)
+	while (1)
 	{
-		ssize_t total = 0;
+		r = read(fd_from, buf, BUF_SIZE);
+		if (r == -1)
+			print_read_error(av[1]);
+		if (r == 0)
+			break;
 
-		while (total < rbytes)
-		{
-			wbytes = write(fd_to, buf + total, rbytes - total);
-			if (wbytes == -1)
-			{
-				close_fd(fd_from);
-				close_fd(fd_to);
-				print_write_error(argv[2]);
-			}
-			total += wbytes;
-		}
-	}
-	if (rbytes == -1)
-	{
-		close_fd(fd_from);
-		close_fd(fd_to);
-		print_read_error(argv[1]);
+		w = write(fd_to, buf, r);
+		if (w == -1 || w != r)
+			print_write_error(av[2]);
 	}
 
 	close_fd(fd_from);
 	close_fd(fd_to);
 	return (0);
 }
+
