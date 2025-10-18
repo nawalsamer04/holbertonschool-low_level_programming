9c031950d63a699152f0e071098a61021c3b9700
root <root@ip-10-42-162-224.ec2.internal>
2025-10-18 12:56:26 +0000

    Fix header guard in dog.h for task 0

---
+++ more_malloc_free/structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - Basic info about a dog
+ * @name: dog name
+ * @age: dog age
+ * @owner: dog owner
+ *
+ * Description: Defines a simple structure for a dog.
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif /* DOG_H */
+
