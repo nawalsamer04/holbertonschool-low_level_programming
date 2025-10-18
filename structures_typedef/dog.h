ebe580dea62c66446e916e40672fcea27bee7c22
root <root@ip-10-42-162-224.ec2.internal>
2025-10-18 13:21:03 +0000

    Add print_dog function to print struct dog safely

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -16,6 +16,7 @@ struct dog
 	char *owner;
 };
 /* Function prototype for init_dog */
+void print_dog(struct dog *d);
 void init_dog(struct dog *d, char *name, float age, char *owner);
 #endif /* DOG_H */
 
