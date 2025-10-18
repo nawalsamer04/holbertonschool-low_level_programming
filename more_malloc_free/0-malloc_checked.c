921758aa4226c16dbc9dc84a7827bcfe07ad2cbc
root <root@ip-10-42-162-224.ec2.internal>
2025-10-18 12:06:52 +0000

    Implement 0-malloc_checked: exit(98) on failure

---
+++ more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * malloc_checked - allocates memory using malloc
+ * @b: number of bytes to allocate
+ *
+ * Return: pointer to allocated memory
+ *         (exits with status 98 on failure)
+ */
+void *malloc_checked(unsigned int b)
+{
+	void *p;
+
+	p = malloc(b);
+	if (p == NULL)
+		exit(98);
+
+	return (p);
+}
+
