397e851e2241751bda51dbd387fc754c10cd37c7
root <root@ip-10-42-225-153.ec2.internal>
2025-10-18 21:39:12 +0000

    Task 6: implement print_chessboard

---
+++ pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_chessboard - prints an 8x8 chessboard
+ * @a: array of 8-character rows
+ */
+void print_chessboard(char (*a)[8])
+{
+	int r, c;
+
+	for (r = 0; r < 8; r++)
+	{
+		for (c = 0; c < 8; c++)
+			_putchar(a[r][c]);
+		_putchar('\n');
+	}
+}
