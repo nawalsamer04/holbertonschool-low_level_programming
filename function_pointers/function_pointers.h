5c9075af59c4df2ca34283942ef73340fa2d88e9
root <root@ip-10-42-225-153.ec2.internal>
2025-10-18 20:21:01 +0000

    Function pointers: task 1 - array_iterator

--- function_pointers/function_pointers.h
+++ function_pointers/function_pointers.h
@@ -11,5 +11,6 @@
  * Description: If either argument is NULL, the function does nothing.
  */
 void print_name(char *name, void (*f)(char *));
+void array_iterator(int *array, size_t size, void (*action)(int));
 
 #endif /* FUNCTION_POINTERS_H */
