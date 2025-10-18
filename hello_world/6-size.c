c8b81a9f849fedd01f71d721b754f2b96627faf1
root <root@ip-10-42-225-153.ec2.internal>
2025-10-18 21:52:09 +0000

    Fix: Task 6 - print sizes of types

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -1,16 +1,17 @@
 #include <stdio.h>
 
 /**
- * main - Entry point
+ * main - prints the size of various types on the computer it is compiled on
  *
- * Return: Always 0 (Success)
+ * Return: Always 0
  */
 int main(void)
 {
-	printf("Size of a char: 0 byte(s)\n", sizeof(char));
-	printf("Size of an int: 0 byte(s)\n", sizeof(int));
-	printf("Size of a long int: 0 byte(s)\n", sizeof(long int));
-	printf("Size of a long long int: 0 byte(s)\n", sizeof(long long int));
-	printf("Size of a float: 0 byte(s)\n", sizeof(float));
+	printf("Size of a char: %zu byte(s)\n", sizeof(char));
+	printf("Size of an int: %zu byte(s)\n", sizeof(int));
+	printf("Size of a long int: %zu byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %zu byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %zu byte(s)\n", sizeof(float));
 	return (0);
 }
+
