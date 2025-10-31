ac34f007689e05457a566f25636e16b6da3e1636
root <root@ip-10-42-55-120.ec2.internal>
2025-10-31 14:30:19 +0000

    fix: final sorted hash table implementation

--- hash_tables/hash_tables.h
+++ hash_tables/hash_tables.h
@@ -5,12 +5,7 @@
 #include <string.h>
 #include <stdio.h>
 
-/**
- * struct hash_node_s - Node of a hash table
- * @key:   string key (unique in the table)
- * @value: value associated with the key (we duplicate it)
- * @next:  pointer to the next node (chaining)
- */
+/* ----- normal hash table structs & prototypes ----- */
 typedef struct hash_node_s
 {
 	char *key;
@@ -18,18 +13,12 @@ typedef struct hash_node_s
 	struct hash_node_s *next;
 } hash_node_t;
 
-/**
- * struct hash_table_s - Hash table data structure
- * @size:  size of the array
- * @array: array of size @size; each cell points to the first node of a list
- */
 typedef struct hash_table_s
 {
 	unsigned long int size;
 	hash_node_t **array;
 } hash_table_t;
 
-/* prototypes required by the checker */
 hash_table_t *hash_table_create(unsigned long int size);
 unsigned long int hash_djb2(const unsigned char *str);
 unsigned long int key_index(const unsigned char *key, unsigned long int size);
@@ -37,17 +26,8 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value);
 char *hash_table_get(const hash_table_t *ht, const char *key);
 void hash_table_print(const hash_table_t *ht);
 void hash_table_delete(hash_table_t *ht);
-#endif /* HASH_TABLES_H */
-/* ===== Sorted hash table types & prototypes (Task 7) ===== */
 
-/**
- * struct shash_node_s - Node of a sorted hash table
- * @key: The key, string (unique in the HashTable)
- * @value: The value corresponding to a key
- * @next: Pointer to the next node of the bucket (collision chain)
- * @sprev: Pointer to the previous element in the sorted linked list
- * @snext: Pointer to the next element in the sorted linked list
- */
+/* ===== Sorted hash table types & prototypes (Task 7) ===== */
 typedef struct shash_node_s
 {
 	char *key;
@@ -57,17 +37,6 @@ typedef struct shash_node_s
 	struct shash_node_s *snext;
 } shash_node_t;
 
-/**
- * struct shash_table_s - Sorted hash table data structure
- * @size: The size of the array
- * @array: An array of size @size
- * @shead: Pointer to the first element of the sorted linked list
- * @stail: Pointer to the last element of the sorted linked list
- *
- * Each cell of @array points to the first node of a bucket linked list
- * (chaining). Additionally, all nodes are connected in a sorted doubly
- * linked list by key (ASCII order) from @shead to @stail.
- */
 typedef struct shash_table_s
 {
 	unsigned long int size;
@@ -76,11 +45,13 @@ typedef struct shash_table_s
 	shash_node_t *stail;
 } shash_table_t;
 
-/* Sorted hash table prototypes */
 shash_table_t *shash_table_create(unsigned long int size);
 int shash_table_set(shash_table_t *ht, const char *key, const char *value);
 char *shash_table_get(const shash_table_t *ht, const char *key);
 void shash_table_print(const shash_table_t *ht);
 void shash_table_print_rev(const shash_table_t *ht);
 void shash_table_delete(shash_table_t *ht);
+
+/* close the header guard */
 #endif /* HASH_TABLES_H */
+
